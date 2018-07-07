6e5809d9e015c9832794360fd68603d1e38085f1
Humberto Banuelos <kaiser1323@gmail.com>
2018-07-07 12:28:25 -0500

    Se agrega proyecto Chapter04Code

---
+++ Chapter04Code/src/main/java/mx/com/hittechs/arrays/ArrayTest.java
@@ -0,0 +1,14 @@
+package mx.com.hittechs.arrays;
+
+/**
+ *
+ * @author Hittechs Training & Consulting
+ * @web www.hittechs.com.mx
+ * @main contacto@hittechs.com.mx
+ */
+public class ArrayTest {
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Chapter04Code/src/main/java/mx/com/hittechs/arrays/Point.java
@@ -0,0 +1,17 @@
+package mx.com.hittechs.arrays;
+
+/**
+ *
+ * @author Hittechs Training & Consulting
+ * @web www.hittechs.com.mx
+ * @main contacto@hittechs.com.mx
+ */
+public class Point {
+
+    public int x;
+    public int y;
+
+    public void getLocation() {
+        System.out.println("Point [x = " + x + ", y = " + y + "]");
+    }
+}
