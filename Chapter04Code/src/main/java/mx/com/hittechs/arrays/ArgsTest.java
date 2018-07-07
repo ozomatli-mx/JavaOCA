f6a76e2ddf17a22e268013c50d0334cc22a23d6e
Humberto Banuelos <kaiser1323@gmail.com>
2018-07-07 13:04:30 -0500

    Se agrega la clase ArgsTest.

---
+++ Chapter04Code/src/main/java/mx/com/hittechs/arrays/ArgsTest.java
@@ -0,0 +1,13 @@
+package mx.com.hittechs.arrays;
+
+/**
+ *
+ * @author Hittechs Training & Consulting
+ * @web www.hittechs.com.mx
+ * @main contacto@hittechs.com.mx
+ */
+public class ArgsTest {
+    public static void main(String[] args) {
+        System.out.println("Args are: "+ args[0] + args[1]);
+    }
+}
