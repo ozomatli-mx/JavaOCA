989b1b3db9d1d836688df079a25e65020c951526
Humberto Banuelos <kaiser1323@gmail.com>
2018-07-07 12:30:24 -0500

    Se agrega proyecto Chapter04Code

--- HumanResources/src/main/java/mx/com/hittechs/test/HumanResourcesTest.java
+++ HumanResources/src/main/java/mx/com/hittechs/test/HumanResourcesTest.java
@@ -2,7 +2,9 @@ package mx.com.hittechs.test;
 
 /**
  *
- * @author Humberto Banuelos
+ * @author Hittechs Training & Consulting
+ * @web www.hittechs.com.mx
+ * @main contacto@hittechs.com.mx
  */
 public class HumanResourcesTest {
     
