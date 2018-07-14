a3ed2c6198e4b33df6ba4c2238cb54942cb8962a
Humberto Banuelos <kaiser1323@gmail.com>
2018-07-14 09:14:27 -0500

    Se agrega el tema while.

---
+++ Chapter06Code/src/main/java/mx/com/hittechs/whiles/WhileTest.java
@@ -0,0 +1,15 @@
+package mx.com.hittechs.whiles;
+
+/**
+ *
+ * @author Hittechs Training & Consulting
+ * @web www.hittechs.com.mx
+ * @mail contacto@hittechs.com.mx
+ */
+public class WhileTest {
+
+    public static void main(String[] args) {
+        int i = 0;
+
+    }
+}
