7100f677236c161c8cdd7b6123e80691d8e3b200
Humberto Banuelos <kaiser1323@gmail.com>
2018-07-14 10:56:45 -0500

    Se agrega Breks.

---
+++ Chapter06Code/src/main/java/mx/com/hittechs/breaks/BreakTest.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.com.hittechs.breaks;
+
+/**
+ *
+ * @author Hittechs Training & Consulting
+ * @web www.hittechs.com.mx
+ * @mail contacto@hittechs.com.mx
+ */
+public class BreakTest {
+    public static void main(String[] args) {
+        String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+        int today = 4;
+        for (int i = 0; i < days.length; i++) {
+            System.out.println(days[i]);
+        }
+    }
+}
