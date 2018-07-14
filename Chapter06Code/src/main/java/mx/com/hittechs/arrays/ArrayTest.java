6645bc3aa72676bd6064d3cf1186590f701accaf
Humberto Banuelos <kaiser1323@gmail.com>
2018-07-14 10:10:47 -0500

    Se agrega ArrayTest.

---
+++ Chapter06Code/src/main/java/mx/com/hittechs/arrays/ArrayTest.java
@@ -0,0 +1,21 @@
+package mx.com.hittechs.arrays;
+
+/**
+ *
+ * @author Hittechs Training & Consulting
+ * @web www.hittechs.com.mx
+ * @mail contacto@hittechs.com.mx
+ */
+public class ArrayTest {
+
+    public static void main(String[] args) {
+        int[] x = new int[4];
+        x[0] = 5;
+        x[1] = 10;
+        x[2] = 15;
+
+        System.out.println(x[0]);
+        System.out.println(x[1]);
+        System.out.println(x[2]);
+    }
+}
