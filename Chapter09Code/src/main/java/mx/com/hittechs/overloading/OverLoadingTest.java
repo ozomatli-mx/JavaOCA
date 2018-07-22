5d889965bc06fb67d0483d91d28c50d827cc64ba
Humberto Banuelos <kaiser1323@gmail.com>
2018-07-22 11:48:20 -0500

    Se agrega proyecto Chapter09Code.

---
+++ Chapter09Code/src/main/java/mx/com/hittechs/overloading/OverLoadingTest.java
@@ -0,0 +1,18 @@
+package mx.com.hittechs.overloading;
+
+/**
+ *
+ * @author Hittechs Training & Consulting
+ * @web www.hittechs.com.mx
+ * @mail contacto@hittechs.com.mx
+ */
+public class OverLoadingTest {
+
+    public void x(byte b) {
+        System.out.println("one");
+    }
+
+    public void x(byte b) {
+        System.out.println("two");
+    }
+}
---
+++ Chapter09Code/src/main/java/mx/com/hittechs/var/args/VarArgsTest.java
@@ -0,0 +1,14 @@
+package mx.com.hittechs.var.args;
+
+/**
+ *
+ * @author Hittechs Training & Consulting
+ * @web www.hittechs.com.mx
+ * @mail contacto@hittechs.com.mx
+ */
+public class VarArgsTest {
+
+    public static void main(String[] args) {
+      
+    }
+}
