009deedc368767dd2a3511c088d154f7be4ec0e2
kaise <kaise@DESKTOP-NLNE05M>
2018-10-27 11:48:41 -0500

    Se sube JUnit.

---
+++ HumanResources/src/test/java/mx/com/hittechs/model/EmployeeTest.java
@@ -0,0 +1,100 @@
+package mx.com.hittechs.model;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ * @author Hittechs Training & Consulting
+ * @web www.hittechs.com.mx
+ * @mail contacto@hittechs.com.mx
+ */
+public class EmployeeTest {
+
+//    Job job;
+//    Department department;
+//
+//    @Before
+//    public void setUp() {
+//        job = new Job();
+//        job.setId(1);
+//        job.setTitle("Java Programmer");
+//        job.setMinSalary(10_000.00);
+//        job.setMaxSalary(20_000.00);
+//        department = new Department();
+//        department.setId(1);
+//        department.setLocation("Mexico");
+//        department.setTitle("Techlonogy Information");
+//    }
+//
+//    @Test
+//    public void test_salary_is_not_less_than_min_salary_in_job() {
+//        Employee employee = new Employee();
+//        employee.setJob(job);
+//        employee.setSalary(9_999.99);
+//        assertEquals(10_000.0, employee.getSalary(), 0.0);
+//    }
+//
+//    @Test
+//    public void test_salary_is_not_greater_than_mix_salary_in_job() {
+//        Employee employee = new Employee();
+//        employee.setJob(job);
+//        employee.setSalary(20_000.01);
+//        assertEquals(20_000.0, employee.getSalary(), 0.0);
+//    }
+//
+//    @Test
+//    public void test_salary_is_equal_than_min_salary_in_job() {
+//        Employee employee = new Employee();
+//        employee.setJob(job);
+//        employee.setSalary(10_000.00);
+//        assertEquals(10_000.0, employee.getSalary(), 0.0);
+//    }
+//
+//    @Test
+//    public void test_salary_is_equal_than_max_salary_in_job() {
+//        Employee employee = new Employee();
+//        employee.setJob(job);
+//        employee.setSalary(20_000.00);
+//        assertEquals(20_000.0, employee.getSalary(), 0.0);
+//    }
+//
+//    @Test
+//    public void test_salary_can_not_be_negative() {
+//        Employee employee = new Employee();
+//        employee.setJob(job);
+//        employee.setSalary(-1.00);
+//        assertEquals(10_000.0, employee.getSalary(), 0.0);
+//    }
+//
+//    @Test
+//    public void test_salary_is_within_range() {
+//        Employee employee = new Employee();
+//        employee.setJob(job);
+//        employee.setSalary(15_000.00);
+//        assertEquals(15_000.0, employee.getSalary(), 0.0);
+//    }
+//    
+//    @Test
+//    public void test_job_is_null() {
+//        Employee employee = new Employee();
+//        employee.setSalary(15_000.00);
+//        assertEquals(0.0, employee.getSalary(), 0.0);
+//    }
+//    @Test
+//    public void test_employee_list_is_up_to_date() {
+//        Employee employee = new Employee();
+//        employee.setDepartment(department);
+//
+//        Employee employeeTwo = new Employee();
+//        employeeTwo.setDepartment(department);
+//
+//        assertNotNull(employee.getDepartment());
+//        assertNotNull(employeeTwo.getDepartment());
+//        assertNotNull(department.getEmployees());
+//
+//        assertEquals(2, department.getEmployees().size());
+//    }
+}
