dab2c7f504aca153069644a10514c07fad2877db
Ozomatli <Ozomatli@DESKTOP-71F5UOJ>
2019-04-10 22:53:57 -0500

    Casting y Promotion, semana 2

---
+++ Chapter03Code/src/main/java/mx/com/hittechs/casting/CastingTest.java
@@ -0,0 +1,12 @@
+
+package mx.com.hittechs.casting;
+
+
+public class CastingTest {
+    
+    public static void main(String[] args) {
+        
+        short v1 = 268;
+        byte v2 = (byte)v1;
+    }
+}
---
+++ Chapter03Code/src/main/java/mx/com/hittechs/primitives/PrimitivesTest.java
@@ -0,0 +1,26 @@
+
+package mx.com.hittechs.primitives;
+
+public class PrimitivesTest {
+    
+    public static void main(String[] args) {
+        
+        //Enteros
+        byte _byte = 127;
+        short _short = 5358;
+        int _int = 22348;
+        long _long = 9223372036854775807L;
+        
+        //Coma flotante
+        float _float = 3.1416F;
+        double _double = 3.1416;
+        
+        //Logicos
+        boolean _boolean = false;
+        
+        //Textuales
+        char c = 'a', c2 = '\u004b', c3 = '\t';
+        char c4 = 64;
+        
+    }
+}
